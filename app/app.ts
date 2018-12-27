e1bcce09a50c0e8ce7fbcdd509a28faddc59eab5
dalongrong <1141591465@qq.com>
2018-12-27 09:58:29 +0800

    init demo project

---
+++ app/app.ts
@@ -0,0 +1,4 @@
+import { test } from "https://unpkg.com/deno_testing@0.0.5/testing.ts";
+import user  from  "./user.ts";
+console.log(test)
+console.log(user)
---
+++ app/user.ts
@@ -0,0 +1,10 @@
+interface User {
+    name:string,
+    age: number
+}
+let user:User;
+user= {
+    name:"dalong",
+    age:444
+}
+export default user
